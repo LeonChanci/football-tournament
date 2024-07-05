fd545a34c5eb8b27d16e18107c9dc22e11695d84
Leon Chanci <leonchanci@gmail.com>
2024-07-05 09:30:12 -0500

    Se integran modificación de pruebas unitarias Se integran ajustes para las pruebas E2E

--- src/football-field/football-field.controller.spec.ts
+++ src/football-field/football-field.controller.spec.ts
@@ -3,21 +3,22 @@ import { FootballField } from './entities/football-field.entity';
 import { FootballFieldController } from './football-field.controller';
 import { FootballFieldService } from './football-field.service';
 import { HttpStatus, NotFoundException } from '@nestjs/common';
+import { CreateFootballFieldDto } from './dto/create-football-field.dto';
+import { UpdateFootballFieldDTO } from './dto/update-football-field.dto';
 
 describe('FootballFieldController', () => {
     let footballFieldController: FootballFieldController;
     let footballFieldService: FootballFieldService;
     let footballFieldModel:  Model<FootballField>;
 
-    beforeEach(()=> {
+    beforeEach(() => {
         footballFieldService = new FootballFieldService(footballFieldModel);
         footballFieldController = new FootballFieldController(footballFieldService);
     })
 
     describe('createFootballFiel', () => {
         it('Debe crear una Cancha de Futbol', async() => {
-            let result = {
-                '_id': '1',
+            let result: CreateFootballFieldDto = {
                 'name': 'Cancha 1',
                 'type': 'Fútbol 11',
                 'address': 'CR 50'
@@ -33,7 +34,6 @@ describe('FootballFieldController', () => {
     describe('getAllFootballFields', () => {
         it('Debe devolver una lista de Canchas de Futbol', async() => {
             let result = [{
-                '_id': '1',
                 'name': 'Cancha 1',
                 'type': 'Fútbol 11',
                 'address': 'CR 50'
@@ -48,7 +48,6 @@ describe('FootballFieldController', () => {
     describe('getFootballFieldById', () => {
         it('Debe devolver una Cancha de Futbol', async() => {
             let result = {
-                '_id': '1',
                 'name': 'Cancha 1',
                 'type': 'Fútbol 11',
                 'address': 'CR 50'
@@ -57,6 +56,7 @@ describe('FootballFieldController', () => {
 
             expect(await footballFieldController.getFootballFieldById('1')).toBe(result);
             expect(findOneSpy).toHaveBeenCalledTimes(1);
+            expect(findOneSpy).toHaveBeenCalledWith('1');
         });
 
         it('Debe devolver una Excepcion, no existe Cancha de Futbol', async() => {
@@ -69,19 +69,18 @@ describe('FootballFieldController', () => {
             }
 
             expect(findOneSpy).toHaveBeenCalledTimes(1);
+            expect(findOneSpy).toHaveBeenCalledWith('1');
         });
     });
 
     describe('updateFootballFiel', () => {
         it('Debe actualizar una Cancha de Futbol', async() => {
-            let update = {
-                '_id': '1',
+            let update: UpdateFootballFieldDTO = {
                 'name': 'Cancha 2',
                 'type': 'Fútbol 5',
                 'address': 'CR 25'
             };
             let result = {
-                '_id': '1',
                 'name': 'Cancha 1',
                 'type': 'Fútbol 11',
                 'address': 'CR 50'
@@ -92,13 +91,13 @@ describe('FootballFieldController', () => {
             
             expect(await footballFieldController.updateFootballFiel('1', update)).toBe(update);
             expect(updateSpy).toHaveBeenCalledTimes(1);
+            expect(updateSpy).toHaveBeenCalledWith('1', update);
         });
     });
 
     describe('deleteFootballFiel', () => {
         it('Debe eliminar una Cancha de Futbol', async() => {
             let result = {
-                '_id': '1',
                 'name': 'Cancha 1',
                 'type': 'Fútbol 11',
                 'address': 'CR 50'
@@ -112,6 +111,7 @@ describe('FootballFieldController', () => {
 
             expect(await footballFieldController.deleteFootballFiel('1')).toBe(delet);
             expect(deleteSpy).toHaveBeenCalledTimes(1);
+            expect(deleteSpy).toHaveBeenCalledWith('1');
         });
 
         it('No debe eliminar una Cancha de Futbol', async() => {
@@ -124,6 +124,7 @@ describe('FootballFieldController', () => {
                 expect(error).toBeInstanceOf(NotFoundException);
             }
             expect(deleteSpy).toHaveBeenCalledTimes(1);
+            expect(deleteSpy).toHaveBeenCalledWith('1');
         });
     });
 });
